208cfc07880ef795cd0129700faeaf08f78ea6fe
Dinesh Kumar Khadka <imkdkumar@outlook.com>
2022-09-25 18:40:08 +0200

    LCM

--- edf.py
+++ edf.py
@@ -1,24 +1,47 @@
 # load task from file
+
+
+from math import gcd 
+
+
 def load_task():
-    temp = []
+    lines = []
+    tasks = []
     #rawdata = open('asset/tasks.txt', 'r')
     #rawdata.seek(45)
     tt_data = open('asset/tt.txt','r')
     tt_data.seek(10)
-    lines = tt_data.readlines()
-    
+    data = tt_data.readlines()
+    for d in data:
+        lines.append(d.strip('\n'))
     for line in lines:
-        temp.append(line.strip('\n'))
+        tasks.append(list(line.split(",")))
+     
+    return tasks
     
+def getLcm(tasks):
+    temp  = []
     
+    for i in range(len(tasks)):
+        temp.append(int(tasks[i][2]))
+    lcm = temp[0]
+    for t in temp[1:]:
+        lcm = int(lcm * t/gcd(lcm, t))
+
+    return lcm
     
-    print(temp)
-def getLcm():
-    pass
     
 
 def edf():
     pass
-    
 
-load_task()
+
+
+
+
+
+
+tasks = load_task()
+print(tasks)
+lcm = getLcm(tasks)
+print(lcm)
