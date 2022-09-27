f89d5617d7bf7c786b14f13d314e6f37746699c9
Dinesh Kumar Khadka <imkdkumar@outlook.com>
2022-09-27 10:45:56 +0200

    edf

--- libraries/EDFalgorithm.py
+++ libraries/EDFalgorithm.py
@@ -1,47 +1,54 @@
 # load task from file
 
 
+from ast import main
 from math import gcd 
+from dataloader import DataLoader as dl
 
-
-def load_task():
-    lines = []
-    tasks = []
-    #rawdata = open('asset/tasks.txt', 'r')
-    #rawdata.seek(45)
-    tt_data = open('asset/tt.txt','r')
-    tt_data.seek(10)
-    data = tt_data.readlines()
-    for d in data:
-        lines.append(d.strip('\n'))
-    for line in lines:
-        tasks.append(list(line.split(",")))
-     
-    return tasks
+class EDF:
+    def __init__(self) -> None:
+        pass
     
-def getLcm(tasks):
-    temp  = []
+    def getLcm(tasks):
+        temp  = []
+        
+        for i in range(len(tasks)):
+            temp.append(int(tasks[i][2]))
+        lcm = temp[0]
+        for t in temp[1:]:
+            lcm = int(lcm * t/gcd(lcm, t))
+
+        return lcm
+        
+        
+
+    def edf():
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    tasks = dl("./test_cases/inf_10_10/taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv")
+    print(tasks.loadFile())
     
-    for i in range(len(tasks)):
-        temp.append(int(tasks[i][2]))
-    lcm = temp[0]
-    for t in temp[1:]:
-        lcm = int(lcm * t/gcd(lcm, t))
 
-    return lcm
+
+
+
+#def load_task():
+#     lines = []
+#     tasks = []
+#     #rawdata = open('asset/tasks.txt', 'r')
+#     #rawdata.seek(45)
+#     tt_data = open('asset/tt.txt','r')
+#     tt_data.seek(10)
+#     data = tt_data.readlines()
+#     for d in data:
+#         lines.append(d.strip('\n'))
+#     for line in lines:
+#         tasks.append(list(line.split(",")))
     
-    
-
-def edf():
-    pass
-
-
-
-
-
-
-
-tasks = load_task()
-print(tasks)
-lcm = getLcm(tasks)
-print(lcm)
+#     return tasks
+        
