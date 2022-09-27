e62598e30b94f400d2a0ccbca312ffb01e34dd74
Alessandro212977 <s212977@dtu.dk>
2022-09-27 09:58:42 +0200

    data loading

--- libraries/tasks.py
+++ libraries/tasks.py
@@ -1,8 +1,16 @@
 class Event:
-    pass
+    def __init__(self, name, duration, period, deadline) -> None:
+        self.name = name
+        self.duration = duration
+        self.period = period
+        self.deadline = deadline
 
 class TT(Event):
-    pass
+    def __init__(self, name, duration, period, deadline) -> None:
+        super().__init__(name, duration, period, deadline)
+    
 
 class ET(Event):
-    pass
+    def __init__(self, name, duration, period, deadline, priority) -> None:
+        super().__init__(name, duration, period, deadline)
+        self.priority = priority
