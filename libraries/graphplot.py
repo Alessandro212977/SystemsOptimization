1037023085461f1d6ed8bc674bcf36e08a2388a7
Dinesh Kumar Khadka <imkdkumar@outlook.com>
2022-11-06 16:37:41 +0100

    commit

--- libraries/graphplot.py
+++ libraries/graphplot.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict
 from libraries.algorithms import *
+from numpy import arange
+    
+    
 
 def plotTTtask(TT, sigma):
     plt.rcParams["figure.figsize"] = [20, 7]
@@ -67,14 +70,45 @@ def _get_cmap(n, name='hsv'):
             
 def plotSimulatedAnealing():
     
+    # objective function
     
-    pass
+    # define range for input
+    r_min, r_max = -5.0, 5.0
+    # sample input range uniformly at 0.1 increments
+    inputs = arange(r_min, r_max, 0.1)
+    # compute targets
+    results = [_objective([x]) for x in inputs]
+    # create a line plot of input vs result
+    plt.plot(inputs, results)
+    # define optimal input value
+    x_optima = 0.0
+    # draw a vertical line at the optimal input
+    plt.axvline(x=x_optima, ls='--', color='red')
+    # show the plot
+    plt.show()
+    
 
+def _objective(x):
+        return x[0]**2.0
+
+def run(name):
+    path = "./test_cases/inf_10_10/taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv"
+    dl = DataLoader(path)
+    
+    if name == "TT":
+        TT, ET  = dl.loadFile()
+        sigma, WCRT = EDF(TT)
+        plotTTtask(TT, sigma)
+    else:
+        plotSimulatedAnealing()
+        
+    
 if __name__ == "__main__":
     path = "./test_cases/inf_10_10/taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv"
     dl = DataLoader(path)
     TT, ET  = dl.loadFile()
     sigma, WCRT = EDF(TT)
-    plotTTtask(TT, sigma)
+    #plotTTtask(TT, sigma)
+    plotSimulatedAnealing()
     
         
