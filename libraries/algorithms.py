58709172b51e72f9d1232ded69443eabcb971ac4
Alessandro212977 <s212977@dtu.dk>
2022-11-08 12:02:58 +0100

    some changes

--- libraries/algorithms.py
+++ libraries/algorithms.py
@@ -2,6 +2,71 @@ from libraries.tasks import PollingServer
 import libraries.dataloader as dataloader
 from math import lcm, ceil
 
+def EDF2(tasks):
+    # LCM of TT task periods
+    T = lcm(*[obj.period for obj in tasks] )
+    t=0
+    sigma = ["idle"]*T
+    R = [0]*len(tasks)
+    WCRT = [0]*len(tasks)
+    C = [obj.duration for obj in tasks]
+    D = [obj.deadline for obj in tasks]
+    #N = [obj.name for obj in tasks]gi
+    D_copy = [obj.deadline for obj in tasks]
+    deadline_flag = True
+    print(WCRT)
+    while t < T:
+        for i, task in enumerate(tasks):
+            if C[i] > 0 and D[i] <= t:
+                #print("t:", t, "deadline:", D[i], "C[i]:", C[i])
+                deadline_flag = False
+                if t-R[i] >= WCRT[i]:
+                    WCRT[i] = t - R[i]
+                #return [], []
+            if C[i] == 0 and D[i] >= t:
+                if t-R[i] >= WCRT[i]:
+                    WCRT[i] = t - R[i]
+                    #print("WCRT:", WCRT[i])
+
+            if t%task.period == 0:
+                R[i] = t
+                C[i] = task.duration 
+                D[i] = t + task.deadline
+                D_copy[i] = t + task.deadline
+                
+                #print("t:", t, "Im here task:", i, "deadline", task.deadline)
+                
+        if all(v == 0 for v in C):
+            sigma[t] = "Idle"
+        else:
+            earliest_deadline = min(D_copy)
+            deadline_index = D_copy.index(earliest_deadline)
+            if(C[deadline_index] > 0):
+                #if (N[deadline_index] == 'polling_server'):
+                #    self.poll_server.getTask()
+                sigma[t] = deadline_index
+                C[deadline_index] -= 1  
+                #print("deadline index:", deadline_index, "C", C[deadline_index], D_copy)
+                #print(" index of task:", earliest_deadline, D.index(earliest_deadline), D.index(D[deadline_index]), D)
+                #print()
+                if (C[deadline_index] == 0):
+                    #print("reset", C[deadline_index])
+                    D_copy[deadline_index] = 100000000   
+            else:
+                #print("im inside the else", deadline_index)
+                D_copy[deadline_index] = 100000000
+                earliest_deadline = min(D_copy)
+                deadline_index = D_copy.index(earliest_deadline)
+                sigma[t] = deadline_index
+                C[deadline_index] -= 1
+        t += 1
+
+    print("after while", WCRT)    
+    if all(v > 0 for v in C) or not deadline_flag:
+        return False, WCRT
+    
+    return sigma, WCRT
+
 def EDF(tasks):
     # LCM of TT task periods
     T = lcm(*[obj.period for obj in tasks] )
--- libraries/simulationAnealing.py
+++ libraries/simulationAnealing.py
@@ -8,9 +8,10 @@ from libraries.algorithms import EDF, EDP
 import libraries.dataloader as dataloader
 from libraries.tasks import PollingServer
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 class SimulatedAnnealing:
-    def __init__(self, TTtasks, pollingserver, initialTemp=0.2, finalTemp=0, tempReduction='linear', iterationPerTemp=10, alpha=0.01, beta=5):
+    def __init__(self, TTtasks, pollingserver, initialTemp=0.001, finalTemp=0, tempReduction='linear', iterationPerTemp=10, alpha=0.0001, beta=5):
         self.solution = pollingserver
         self.currTemp = initialTemp
         self.finalTemp = finalTemp
@@ -45,18 +46,21 @@ class SimulatedAnnealing:
         return self.currTemp <= self.finalTemp or self.neighborOperator(self.solution) == 0
 
     def run(self):
+        costs_plot = []
+        accepted_costs_plot = []
+        durations_plot = []
+        bins = []
+        bins_scatter = []
+
         while not self.isTerminationCriteriaMet():
-            # iterate that number of times
+            print("TEMPERATURE:", self.currTemp)
             for i in range(self.iterationPerTemp):
-                # get all of the neighbors
-                neighbors = self.neighborOperator(self.solution)
                 # pick a random neighbor
-                newSolution = random.choice(neighbors)
+                newSolution = self.neighborOperator(self.solution)
                 schedule, responsetime = EDP(newSolution)
                 sigma, wrct= EDF(self.TTtasks+[newSolution])
                 while(schedule != True or not sigma):
-                    neighbors.remove(newSolution)
-                    newSolution = random.choice(neighbors)
+                    newSolution = self.neighborOperator(self.solution)
                     schedule, responsetime = EDP(newSolution)
                     sigma, wrct= EDF(self.TTtasks+[newSolution])
 
@@ -65,36 +69,54 @@ class SimulatedAnnealing:
                 schedule, responsetime = EDP(self.solution)
                 sigma, wrctNewSolution= EDF(self.TTtasks+[newSolution])
                 schedule, responsetimeNew = EDP(newSolution)
-                wcrt_norm = preprocessing.normalize([np.array([np.mean(wrctSolution), np.mean(wrctNewSolution)])])[0]
-                response_norm = preprocessing.normalize([np.array([responsetime, responsetimeNew])])[0]
-                cost = 0.6*(wcrt_norm[0] - wcrt_norm[1]) +0.4*(response_norm[0] - response_norm[1])
-                # if the new solution is better, accept it
-                if -cost > 0:
-                    self.solution = newSolution
-                    print("this solution is better than the old one")
-                # if the new solution is not better, accept it with a probability of e^(-cost/temp)
-                elif random.uniform(0, 1) < math.exp(-cost / self.currTemp):
-                    print("this solution is not better but it got accepted prob:", math.exp(-cost / self.currTemp), "temp:", self.currTemp)
-                    self.solution = newSolution
 
-                print(i, self.solution, "cost:", cost)
-            # decrement the temperature
+                cost = self.ComputeCost(wrctNewSolution, responsetimeNew) - self.ComputeCost(wrctSolution, responsetime)
+                print("cost: {}, new duration: {}, new period {}, acceptance prob: {}".format(cost, newSolution.duration, newSolution.period, math.exp(cost / self.currTemp)))
+                costs_plot.append(self.ComputeCost(wrctNewSolution, responsetimeNew))
+                durations_plot.append(newSolution.duration)
+                bins.append(bins[-1]+1 if bins else 0)
+                
+                # if the new solution is better, accept it
+                if cost < 0 or self.acceptance(cost):
+                    self.solution = newSolution
+                    accepted_costs_plot.append(self.ComputeCost(wrctNewSolution, responsetimeNew))
+                    bins_scatter.append(bins[-1])
+
+
             self.decrementRule()
 
+        print(responsetime)
+        plt.plot(bins, costs_plot)
+        plt.scatter(bins_scatter, accepted_costs_plot)
+        #plt.plot(bins[1:], durations_plot)
+        plt.show()
+
+    def acceptance(self, cost):
+        if cost > 0:
+            print("the solution is accepted with prob", math.exp(-cost/self.currTemp))
+            return random.random() < math.exp(-cost/self.currTemp)
+        return False
+
+    def ComputeCost(self, wcrt, response, weight=0.6):
+        #wcrt_norm = preprocessing.normalize([np.array([np.mean(wcrtNew), np.mean(wcrtOld)])])[0]
+        #print(np.mean(wcrtNew), np.mean(wcrtOld), wcrt_norm)
+        #response_norm = preprocessing.normalize([np.array([responseNew, responseOld])])[0]
+
+        norm = preprocessing.normalize([np.array([np.mean(wcrt), response])])[0]
+        return weight*np.mean(norm[0]) + (1-weight)*norm[1]
+        return (response_norm[0] - response_norm[1])
+        return (wcrt_norm[0] - wcrt_norm[1]) #+ (1-weight)*(response_norm[0] - response_norm[1])
+
     def neighborOperator(self, center):
-        neighbors = []
-        for i in [-10, 10]:
-            #for j in [-10, 10]:
-            #    for k in [-10, 10]:
-            neighbor = PollingServer(center.duration + i, center.period, center.deadline, center.tasks)
-            neighbors.append(neighbor)
-        return neighbors
+        duration_offset = random.randint(-20, 20)#*10
+        period_offset = 0#random.randint(-3, 3)*100
+        return PollingServer(center.duration + duration_offset, center.period + period_offset, center.deadline, center.tasks)
 
 if __name__ == "__main__":
     path = "./test_cases/inf_10_10/taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv"
     dl = dataloader.DataLoader(path)
     TT, ET  = dl.loadFile()
-    TT, ET = TT[:3], ET[:3]
+    TT, ET = TT[:10], ET[:10]
     init_ps = PollingServer(1800, 2000, 2000, ET)
     sa = SimulatedAnnealing(TT, init_ps)
     sa.run()
