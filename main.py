3dce55d1fb4e764e7559fa61b3afa68534ea2850
alessandro-canevaro <alessandrocanevaro26@gmail.com>
2022-11-26 17:58:11 +0100

    bars + gitignore

--- main.py
+++ main.py
@@ -14,20 +14,22 @@ def experiment(data_path, profiling=False):
     dl = dataloader.DataLoader(data_path)
     TT, ET = dl.loadFile()
 
-    #optim = SimulatedAnnealing(TT, ET, numinstances=4, numworkers=4, maxiter=200)
-    optim = GeneticAlgorithm(TT, ET, numinstances=2, numworkers=2, maxiter=50, pop_size=16, num_parents=4)
+    optim = SimulatedAnnealing(TT, ET, numinstances=4, numworkers=4, maxiter=200)
+    #optim = GeneticAlgorithm(TT, ET, numinstances=2, numworkers=1, maxiter=10, pop_size=16, num_parents=4)
     # optim.plotTemperature()
+
+    bar_iter = optim.maxIter*optim.numInstances if optim.numWorkers==1 else optim.numInstances
 
     if profiling:
         import cProfile, pstats
 
         with cProfile.Profile() as pr:
-            with tqdm(total=optim.maxIter, desc="Iterations") as bar:  # progress bar
+            with tqdm(total=bar_iter, desc="Progress", bar_format="{desc}{percentage:3.0f}%|{bar:10}{r_bar}") as bar:  # progress bar
                 optim.run(bar.update)
         pr = pstats.Stats(pr)
         pr.sort_stats("cumulative").print_stats(10)
     else:
-        with tqdm(total=optim.maxIter, desc="Iterations") as bar:
+        with tqdm(total=bar_iter, desc="Progress", bar_format="{desc}{percentage:3.0f}%|{bar:10}{r_bar}") as bar:
             optim.run(bar.update)
 
 
