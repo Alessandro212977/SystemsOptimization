f068674abe0af4dac2683d415a365d7afd81dd29
Alessandro212977 <s212977@dtu.dk>
2022-11-29 09:30:46 +0100

    merge

--- main.py
+++ main.py
@@ -8,77 +8,44 @@ from libraries.optimizers import (
 from libraries.algorithms import EDF, EDP
 from libraries.graphplot import getTimetablePlot
 
+def experiment(data_path, profiling=False):
 
-def SA(TT, ET):
-    sa = SimulatedAnnealing(TT, ET, numinstances=4, numworkers=4, maxiter=1000)
-    # sa.plotTemperature()
-    sa.printSolution()
+    # load tasks
+    dl = dataloader.DataLoader(data_path)
+    TT, ET = dl.loadFile()
 
-    if False:
+    optim = SimulatedAnnealing(TT, ET, numinstances=4, numworkers=4, maxiter=800, toll=0.25)
+    #optim = GeneticAlgorithm(TT, ET, numinstances=2, numworkers=1, maxiter=10, pop_size=16, num_parents=4)
+    # optim.plotTemperature()
+
+    bar_iter = optim.maxIter*optim.numInstances if optim.numWorkers==1 else optim.numInstances
+
+    if profiling:
         import cProfile, pstats
 
         with cProfile.Profile() as pr:
-            with tqdm(total=sa.maxIter, desc="Iterations") as bar:  # progress bar
-                sa.run(bar.update)
+            with tqdm(total=bar_iter, desc="Progress", bar_format="{desc}{percentage:3.0f}%|{bar:10}{r_bar}") as bar:  # progress bar
+                optim.run(bar.update)
         pr = pstats.Stats(pr)
         pr.sort_stats("cumulative").print_stats(10)
     else:
-        with tqdm(total=sa.maxIter, desc="Iterations") as bar:
-            sa.run(bar.update)
-
-    schedulable_TT, timetable, wcrt_TT, __ = EDF(TT + sa.bestSolution)
-    print(wcrt_TT)
-    for ps in sa.bestSolution:
-        schedulable_ET, wcrt_ET, __ = EDP(ps)
-        print(wcrt_ET)
-
-    sa.printSolution()
-    sa.plotCost()
-
-    getTimetablePlot(TT + sa.bestSolution, timetable, group_tt=True).show()
+        with tqdm(total=bar_iter, desc="Progress", bar_format="{desc}{percentage:3.0f}%|{bar:10}{r_bar}") as bar:
+            optim.run(bar.update)
 
 
-def GA(TT, ET):
-    ga = GeneticAlgorithm(TT, ET, numinstances=1, numworkers=1, maxiter=100, pop_size=16, num_parents=4)
+    optim.printSolution()
+    optim.plotBars()
+    optim.plotCost(instance_idx="all")
 
-    if False:
-        import cProfile, pstats
-
-        with cProfile.Profile() as pr:
-            with tqdm(total=ga.maxIter, desc="Iterations") as bar:  # progress bar
-                ga.run(bar.update)
-        pr = pstats.Stats(pr)
-        pr.sort_stats("cumulative").print_stats(10)
-    else:
-        with tqdm(total=ga.maxIter, desc="Iterations") as bar:
-            ga.run(bar.update)
-
-    schedulable_TT, timetable, wcrt_TT, __ = EDF(TT + ga.bestSolution)
-    print(wcrt_TT)
-    for ps in ga.bestSolution:
-        schedulable_ET, wcrt_ET, __ = EDP(ps)
-        print(wcrt_ET)
-
-    ga.printSolution()
-    ga.plotCost()
-
-    getTimetablePlot(TT + ga.bestSolution, timetable, group_tt=True).show()
+    __, timetable, __, __ = EDF(TT + optim.bestSolution)
+    getTimetablePlot(TT + optim.bestSolution, timetable, group_tt=True).show()
 
 
-def main():
+if __name__ == "__main__":
     import cpuinfo
-
     cpu = cpuinfo.get_cpu_info()
     print("{}, {} cores".format(cpu["brand_raw"], cpu["count"]))
 
     path = "./test_cases/taskset_small.csv"
     # path = "./test_cases/taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv"
-    dl = dataloader.DataLoader(path)
-    TT, ET = dl.loadFile()
-
-    #SA(TT, ET)
-    GA(TT, ET)
-
-
-if __name__ == "__main__":
-    main()
+    experiment(path)
